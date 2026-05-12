c9885d8b1e3471a7a5260baeabad0f4cee33037f
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-05-12 10:02:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h> // Includes the "Standard Input Output" library
 
 int main() {
-    int x = 0
+    int x = 0;
     for (int n = 0, i == n, n++) {
         x++
     }
-    return x
+    return x;
 }
