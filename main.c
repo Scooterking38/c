62ab89150d399b7dea79fd8458ec3a7f0e293595
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-05-12 09:54:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,6 @@
-__declspec(dllexport) int add(int a, int b) { return a + b; }
+#include <stdio.h> // Includes the "Standard Input Output" library
+
+int main() {
+    printf("Hello World!\n"); // \n is a new line
+    return 0; // 0 means "The program finished successfully"
+}
